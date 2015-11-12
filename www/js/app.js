22cce614d83c59ba4362c66624bb4f186077825f
avery.duffin@nov.com <avery.duffin@nov.com>
2015-11-12 15:08:24 -0600

    fixed database problem

--- www/js/app.js
+++ www/js/app.js
@@ -13,16 +13,24 @@ angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    if (window.cordova) {
+      //cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      //cordova.plugins.Keyboard.disableScroll(true);
 
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
-	db = $cordovaSQLite.openDB("my.db");
+	if (window.cordova) {
+		console.log('device')
+		//db = $cordovaSQLite.openDB({ name: "my.db" }); //device
+		db = window.openDatabase("my.db", '1', 'my', 1024 * 1024 * 100);
+	}else{
+		console.log('browser')
+		db = window.openDatabase("my.db", '1', 'my', 1024 * 1024 * 100); // browser
+	}
+	//db = $cordovaSQLite.openDB("my.db");
 	$cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS people (id integer primary key, firstname text, lastname text)");
   });
 })
