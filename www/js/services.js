c005eea3a57bdfd944a3e8cdb527d3719567fdcd
avery.duffin@nov.com <avery.duffin@nov.com>
2015-11-19 18:33:27 -0600

    Removed the database to add the working copy

--- www/js/services.js
+++
@@ -1,8 +0,0 @@
-angular.module('starter.services', [])
-
-.factory('Participants', function ($resource) {
-	return null//$resource('http://www.derricksanddiamonds.org/api/index.php/participants/:personId', {personId: '@id'}, {
-    //return $resource('http://localhost:8086/Slim/index.php/participants/:personId', {personId: '@id'}, {
-  //create: {method:'POST'}
- //});
-});
